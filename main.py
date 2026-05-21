5d6cd0f02818cfdb75a5cd90ab7ace87de46f8f0
MarioDev <maile722@googlemail.com>
2026-05-21 17:12:03 +0200

    Integrate Twilio SMS and OpenWeatherMap API
    Refactor code to use Twilio for SMS notifications and OpenWeatherMap for weather data. Removed email functionality and added environment variable configurations.

--- main.py
+++ main.py
@@ -1,38 +1,84 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
+from twilio.rest import Client
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+MY_LAT = os.environ.get("MY_LAT")
+MY_LONG = os.environ.get("MY_LONG")
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+API_KEY = os.environ.get("OWN_API_KEY")
+MY_NUMBER = os.environ.get("MY_NUMBER")
+VIRTUAL_NUMBER = os.environ.get("VIRTUAL_NUMBER")
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+weather_params = {
+    'lat': MY_LAT,
+    'lon': MY_LONG,
+    'appid': API_KEY,
+    'count': 4,
+    'units': 'metric',
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+
+# check in if rains and sent SMS
+hour_counter = 0
+temp_list = []
+weather_id_list = []
+
+"""collects todays temperature"""
+for hour_data in range(0,6):
+    temp_min = weather_data['list'][hour_counter]['main']['temp_min']
+    temp_max = weather_data['list'][hour_counter]['main']['temp_max']
+    hour_counter += 1
+    temp_list.append(temp_min)
+    temp_list.append(temp_max)
+
+todays_max_temp = max(temp_list)
+todays_min_temp = min(temp_list)
+
+
+"""check if it will rain today"""
+hour_counter = 0
+weather_id_list = []
+
+for hour_data in range(0,6):
+    weather_id = weather_data['list'][hour_counter]['weather'][0]['id']
+    hour_counter += 1
+    weather_id_list.append(weather_id)
+
+
+id_for_rain = 700
+counter = 0
+
+for w_i in weather_id_list:
+    if w_i >= id_for_rain:
+        counter += 1
+if counter >= 1:
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    message = client.messages.create(
+        body=f"Heute werden es zwischen {todays_min_temp}°C und {todays_max_temp}°C. Du brauchst heute keinen ☂️.",
+        from_=f"{VIRTUAL_NUMBER}",
+        to=f"{MY_NUMBER}",
+    )
+else:
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    message = client.messages.create(
+        body=f"Heute werden es zwischen {todays_min_temp}°C und {todays_max_temp}°C.Heute brauchst du einen ☂️.",
+        from_=f"{VIRTUAL_NUMBER}",
+        to=f"{MY_NUMBER}",
+    )
+
+
+
+
+
+
+
+
+
+
